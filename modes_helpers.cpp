7785b001daf8fb2c1daabb0a82483317528b1b40
cselicato <selicatocarmen@gmail.com>
2025-08-18 11:00:14 +0200

    Fixed issue #5 (for real this time)

--- modes_helpers.cpp
+++ modes_helpers.cpp
@@ -189,14 +189,14 @@ TTree * make_data_tree(vector<vector<string>>& data, const modes& mode, stored_v
         {
             cout << "Acquisition mode: Spectroscopy." << endl;
 
-            Double_t time = 0;
             reset<int16_t>(v.data_type, v);
             reset<Int_t>(v.LG, v);
             reset<Int_t>(v.HG, v);
 
-            for (vector<string> row : data){
+            for (int i=0; i<data.size(); i++){
+                vector<string> row = data[i];
                 v.Trg_Id = stoi(row[1]);
-                v.TStamp = stold(row[0]);
+                v.TStamp = stod(row[0]);
                 v.hits = stoi(row[3]);
                 v.ch_mask = stoull(row[4], nullptr, 16);
                 
@@ -209,8 +209,12 @@ TTree * make_data_tree(vector<vector<string>>& data, const modes& mode, stored_v
                 v.LG[board][ch_ID] = stoi(row[7]);
                 v.HG[board][ch_ID] = stoi(row[8]);
 
-                if (v.TStamp != time){
-                    time = v.TStamp;
+                if (i==data.size()-1){
+                    t->Fill();
+                    continue;
+                }                
+
+                else if (v.TStamp != stod(data[i+1][0])){
                     t->Fill();
                     reset<int16_t>(v.data_type, v);
                     reset<Int_t>(v.LG, v);
@@ -224,17 +228,17 @@ TTree * make_data_tree(vector<vector<string>>& data, const modes& mode, stored_v
         {
             cout << "Acquisition mode: Spect_Timing." << endl;
 
-            Double_t time = 0;
             reset<int16_t>(v.data_type, v);
             reset<Int_t>(v.LG, v);
             reset<Int_t>(v.HG, v);
             reset<float>(v.ToA, v);
             reset<float>(v.ToT, v);
 
-            for (vector<string> row : data){
+            for (int i=0; i<data.size(); i++){
+                vector<string> row = data[i];
                 v.Trg_Id = stoull(row[1]);
                 v.ch_mask = stoull(row[4], nullptr, 16);
-                v.TStamp = stold(row[0]);
+                v.TStamp = stod(row[0]);
                 v.hits = stoi(row[3]);
 
                 board= stoi(row[2]);
@@ -248,8 +252,12 @@ TTree * make_data_tree(vector<vector<string>>& data, const modes& mode, stored_v
                 v.ToA[board][ch_ID] = stof(row[9]);
                 v.ToT[board][ch_ID] = stof(row[10]);
 
-                if (v.TStamp != time){
-                    time = v.TStamp;
+                if (i==data.size()-1){
+                    t->Fill();
+                    continue;
+                } 
+
+                else if (v.TStamp != stod(data[i+1][0])){
                     t->Fill();
                     reset<int16_t>(v.data_type, v);
                     reset<Int_t>(v.LG, v);
@@ -268,13 +276,13 @@ TTree * make_data_tree(vector<vector<string>>& data, const modes& mode, stored_v
             // the acquistion modes Timing_CStart and Timing_CStop have the same structure
             cout << "The acquisition mode is Timing." << endl;
 
-            Double_t time = 0;
             reset<int16_t>(v.data_type, v);
             reset<float>(v.ToA, v);
             reset<float>(v.ToT, v);
 
-            for (vector<string> row : data){
-                v.TStamp = stold(row[0]);
+            for (int i=0; i<data.size(); i++){
+                vector<string> row = data[i];
+                v.TStamp = stod(row[0]);
                 v.hits = stoi(row[2]);
 
                 board= stoi(row[1]);
@@ -285,9 +293,13 @@ TTree * make_data_tree(vector<vector<string>>& data, const modes& mode, stored_v
                 v.data_type[board][ch_ID] = stoi(row[4], nullptr, 16);
                 v.ToA[board][ch_ID] = stof(row[5]);    
                 v.ToT[board][ch_ID] = stof(row[6]);   
+        
+                if (i==data.size()-1){
+                    t->Fill();
+                    continue;
+                }
 
-                if (v.TStamp != time){
-                    time = v.TStamp;
+                else if (v.TStamp != stod(data[i+1][0])){
                     t->Fill();
                     reset<int16_t>(v.data_type, v);
                     reset<float>(v.ToA, v);
@@ -301,12 +313,12 @@ TTree * make_data_tree(vector<vector<string>>& data, const modes& mode, stored_v
         {
             cout << "The acquisition mode is Counting." << endl;
 
-            uint64_t ID = 0;
             reset<int64_t>(v.counts, v);
 
-            for (vector<string> row : data){
+            for (int i=0; i<data.size(); i++){
+                vector<string> row = data[i];
                 v.Trg_Id = stoull(row[1]);
-                v.TStamp = stold(row[0]);
+                v.TStamp = stod(row[0]);
                 v.ch_mask = stoull(row[4], nullptr, 16);
                 v.hits = stoi(row[3]);
 
@@ -315,8 +327,12 @@ TTree * make_data_tree(vector<vector<string>>& data, const modes& mode, stored_v
                 is_valid_ind(board, ch_ID,N_boards);
                 v.counts[board][ch_ID] = stoi(row[6]);
 
-                if (v.Trg_Id != ID){
-                    ID = v.Trg_Id;
+                if (i==data.size()-1){
+                    t->Fill();
+                    continue;
+                } 
+
+                else if (v.TStamp != stod(data[i+1][0])){
                     t->Fill();
                     reset<int64_t>(v.counts, v);
                 }
